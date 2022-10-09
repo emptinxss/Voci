ba972bc01eaeed081ec508a22f76cc4e6fe931e6
Miki Leardini <mikileardini96@gmail.com>
2022-10-09 15:21:32 +0200

    Reset database

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\DB;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -16,16 +16,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->call(function () {
-            DB::table('posts')->whereRaw('id > 3')->delete();
-        })->everyMinute();
-        $schedule->call(function () {
-            DB::table('media')->whereRaw('id > 3')->delete();
-        })->everyMinute();
-        $schedule->call(function () {
-            DB::table('authors')->whereRaw('id > 3')->delete();
-        })->everyMinute();
+
+        //reset database
+
+        $schedule->exec('php artisan migrate:fresh --seed')
+            ->daily();
     }
 
     /**
