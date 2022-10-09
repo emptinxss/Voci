4e05172581715e8cd15e8c3b7b4b5abf7757a5c7
Miki Leardini <mikileardini96@gmail.com>
2022-10-09 15:21:45 +0200

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //CUSTOM AUTHORS
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('authors')->insert([
+            'name' => 'Emma',
+            'surname' => 'Miller'
+        ]);
+        DB::table('authors')->insert([
+            'name' => 'Mia',
+            'surname' => 'Jackson'
+        ]);
+
+        // CUSTOM MEDIA
+
+        DB::table('media')->insert([
+            'name' => 'Dance',
+            'category' => 'IMG',
+            'description' => 'Whatever is good for your soul, do that...',
+            'file' => '1665301795.jpg'
+        ]);
+        DB::table('media')->insert([
+            'name' => 'Sand ',
+            'category' => 'IMG',
+            'description' => 'Beautiful place...',
+            'file' => '1665301907.jpg'
+        ]);
+        DB::table('media')->insert([
+            'name' => 'Tea',
+            'category' => 'VIDEO',
+            'description' => 'There is no better way to start the day with a gooog cup of tea!',
+            'file' => '1665302372.mp4'
+        ]);
+
+        //CUSTOM POSTS
+
+        DB::table('posts')->insert([
+            'post_name' => 'Happiness',
+            'author_id' => '1',
+            'media_id' => '1'
+        ]);
+        DB::table('posts')->insert([
+            'post_name' => 'Sand and clouds',
+            'author_id' => '2',
+            'media_id' => '2'
+        ]);
+        DB::table('posts')->insert([
+            'post_name' => 'Tea',
+            'author_id' => '1',
+            'media_id' => '3'
+        ]);
     }
 }
