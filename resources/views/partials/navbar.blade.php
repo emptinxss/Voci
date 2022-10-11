62642ae8fe69689ee1bdc7e27e1a4bf6b6cc329b
Miki Leardini <mikileardini96@gmail.com>
2022-10-11 11:34:59 +0200

    update style

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,17 +3,17 @@
         <div class="flex items-center justify-between border-b-2 border-gray-400 pt-6 md:justify-start md:space-x-10">
             <div class="flex justify-start lg:w-0 lg:flex-1">
                 <a href="/posts">
-                    <span class="text-7xl passion font-medium font-heading tracking-px-n text-black-500">Voci</span>
+                    <span class=" text-7xl passion font-medium font-heading tracking-px-n text-black-500">Voci</span>
                 </a>
             </div>
 
             <div class="items-center justify-end md:flex md:flex-1 lg:w-0 ">
                 <a href="/posts"
-                    class="whitespace-nowrap text-base sm:font-medium text-black-500 hover:underline">POSTS</a>
+                    class="whitespace-nowrap text-xs md:text-base md:font-medium text-black-500 hover:underline">POSTS</a>
                 <a href="/media"
-                    class="whitespace-nowrap text-base sm:font-medium text-black-500 hover:underline ml-4">MEDIA</a>
+                    class="whitespace-nowrap text-xs md:text-base md:font-medium text-black-500 hover:underline ml-1 md:ml-4">MEDIA</a>
                 <a href="/authors"
-                    class="whitespace-nowrap text-base sm:font-medium text-black-500 hover:underline ml-4">AUTHORS</a>
+                    class="whitespace-nowrap text-xs md:text-base md:font-medium text-black-500 hover:underline ml-1 md:ml-4">AUTHORS</a>
             </div>
         </div>
     </div>
--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,6 +1,6 @@
 <div class=" flex justify-end ">
     <form method="GET" class="flex justify-end">
-        <div class="w-full mb-6 md:mb-0 ">
+        <div class=" w-36 sm:w-full mb-6 md:mb-0 ">
             <input
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-xl py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 placeholder="Search..." name="search" value="{{ request('search') }}">
